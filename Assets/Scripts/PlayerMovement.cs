a36099c25fbe60484dde0df312a5c361fea816ab
janidev20 <phjohn2008@gmail.com>
2024-03-07 17:03:08 +0100

    Player Movement Update 1
    yessir

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,28 +11,58 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Player stats")]
 
+    [SerializeField] private float _maxSpeed = 20;
+    [SerializeField] private float _minSpeed;
+
     [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _moveSlide;
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private float _gravity;
+    [SerializeField] private float _currentSpeed; // For changing speed
+    [SerializeField] private float _SprintMultiplier = 0.015f; // For changing speed
+
+    [SerializeField] private float _slideAtSpeed; // The specfic moving speed to start sliding method at
+    [SerializeField] private float _slideTimer;
+    [SerializeField] private bool _slideTimerEnabled;
+    [SerializeField] private float _slideAmount;
+
+
+    [SerializeField] private bool isSprinting;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _minSpeed = _moveSpeed;
     }
 
 
     private void FixedUpdate()
     {
-        float inputX = Input.GetAxis("Horizontal"); // Reference of the left and right input 
-
-        Vector2 movement = new Vector2(inputX * _moveSpeed, 0);
-
-        movement *= Time.deltaTime;
-
-        transform.Translate(movement);
-
+        Movement();
+        Flip();
     }
 
+    void Movement()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+
+
+        rb.velocity = movement.normalized * Time.fixedDeltaTime * _moveSpeed;
+
+        //Sprinting
+        isSprinting = false ? true : Input.GetKey(KeyCode.X);
+    }
+
+    void Flip()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        if (inputX > 0)
+        {
+            spriteRenderer.flipX = true;
+        } else if (inputX < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
 }
- 
