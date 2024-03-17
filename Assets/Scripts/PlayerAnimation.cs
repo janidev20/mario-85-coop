b3f2b7582ffe7e72877807e1d9aa625807b77835
janidev20 <phjohn2008@gmail.com>
2024-03-17 13:58:29 +0100

    huge update wow
    yeah

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] PlayerMovement plyMovement;
+    [SerializeField] Animator playerAnimator;
+
+    [SerializeField] Animator MXAnimator, PCAnimator, FHAnimator, FScrAnimator;
+    [SerializeField] private GameObject MX_Body, PCrawler_Body, isFH_Body;
+
+    [SerializeField] public bool isMX, isPCrawler, isFH;
+
+    [SerializeField] private float coolDownTimer; // This is for switching characters, so that the Y position doesn't bug out.
+    [SerializeField] private float coolDownAmount = 2.5f;
+
+    [SerializeField] private float transformTime = 0.85f;
+    [SerializeField] public bool isTransforming; // If the transform animation is playing (based on time calculation)
+
+    private void Awake()
+    {
+        if (playerAnimator == null)
+        {
+            AnimationMethod();
+        }
+    }
+
+    private void Update()
+    {
+        if (coolDownTimer < coolDownAmount)
+        {
+            coolDownTimer += Time.deltaTime;
+        } 
+        else if (coolDownTimer >= coolDownAmount)
+        {
+            CharacterMethod();
+        }
+
+        if (coolDownTimer < transformTime && !isFH && plyMovement.onGround)
+        {
+            isTransforming = true;
+        }
+        else if (coolDownTimer >= transformTime)
+        {
+            isTransforming = false;
+        }
+
+        AnimatorBooleans();
+        AnimationMethod();
+    }
+
+    void AnimatorBooleans()
+    {
+        if (plyMovement.isRunning)
+        {
+            playerAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isRunning", false);
+        }
+
+        if (plyMovement.isJumping)
+        {
+            playerAnimator.SetBool("isJumping", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isJumping", false);
+        }
+
+        if (plyMovement.isSliding)
+        {
+            playerAnimator.SetBool("isSliding", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isSliding", false);
+        }
+    }
+
+    void CharacterMethod()
+    {
+        if (Input.GetKeyDown(KeyCode.F3) && isPCrawler)
+        {
+            isMX = true;
+            isPCrawler = false;
+            isFH = false;
+            coolDownTimer = 0; // Resets the coolDownTimer
+        }
+        else if (Input.GetKeyDown(KeyCode.F2) && isFH)
+        {
+            isPCrawler = true;
+            isMX = false;
+            isFH = false;
+            coolDownTimer = 0; // Resets the coolDownTimer
+        }
+        else if (Input.GetKeyDown(KeyCode.F1) && !isFH)
+        {
+            isFH = true;
+            isMX = false;
+            isPCrawler = false;
+            FScrAnimator.gameObject.SetActive(false);
+            FScrAnimator.gameObject.SetActive(true);
+            coolDownTimer = 0; // Resets the coolDownTimer
+        }
+
+
+    }
+
+    void AnimationMethod()
+    {
+        
+
+        if (isMX)
+        {
+            playerAnimator = MXAnimator;
+            MX_Body.SetActive(true);
+            PCrawler_Body.SetActive(false);
+            isFH_Body.SetActive(false);
+        }
+        else if (isPCrawler)
+        {
+            playerAnimator = PCAnimator;
+            PCrawler_Body.SetActive(true);
+            MX_Body.SetActive(false);
+            isFH_Body.SetActive(false);
+        }
+        else if (isFH)
+        {
+            playerAnimator = FHAnimator;
+            MX_Body.SetActive(false);
+            PCrawler_Body.SetActive(false);
+            isFH_Body.SetActive(true);
+        }
+    }
+
+  
+
+
+}
---
+++ Assets/StatusChange.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class StatusChange : MonoBehaviour
+{
+    [SerializeField] PlayerAnimation AnimationScript;
+    [SerializeField] TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (AnimationScript.isMX)
+        {
+
+            StartCoroutine(MXText());
+            StopCoroutine(DisguisedText());
+            StopCoroutine(CorruptedDisguiseText());
+
+        } else if (AnimationScript.isPCrawler)
+        {
+            StartCoroutine(CorruptedDisguiseText());
+            StopCoroutine(DisguisedText());
+            StopCoroutine(MXText());
+        } else if (AnimationScript.isFH)
+        {
+            StartCoroutine(DisguisedText());
+            StopCoroutine(CorruptedDisguiseText());
+            StopCoroutine(MXText());
+        }
+    }
+
+    IEnumerator CorruptedDisguiseText()
+    {
+        while (AnimationScript.isPCrawler)
+        {
+                yield return new WaitForSeconds(0.5f);
+
+                text.text = "S̸T̶A̶T̸U̵S̵ : DISGUISE CORR̴U̴P̷T̵E̸D̶";
+
+                yield return new WaitForSeconds(0.50f);
+
+                text.text = "S̸T̶A̶T̸U̵S̵ : DISGUISE CORR̸̬̔U̵͖̇P̴̣͝T̸̤̚E̶͔̿D̵̖͐";
+
+                yield return new WaitForSeconds(0.25f);
+
+
+                // REPEAT
+
+                text.text = "S̸T̶A̶T̸U̵S̵ : DISGUISE CORR̴U̴P̷T̵E̸D̶";
+
+                yield return new WaitForSeconds(0.50f);
+
+                text.text = "S̸T̶A̶T̸U̵S̵ : DISGUISE CORR̸̬̔U̵͖̇P̴̣͝T̸̤̚E̶͔̿D̵̖͐";
+
+                yield return new WaitForSeconds(0.25f);
+
+
+                text.text = "";
+
+                yield return new WaitForSeconds(1.0f);
+            }
+    }
+    
+    IEnumerator DisguisedText()
+    {
+        while (AnimationScript.isFH)
+        {
+                yield return new WaitForSeconds(0.5f);
+
+
+                text.text = "STATUS̶̟̻̩̃̃͜  : DISGUISED AS MARIO";
+
+                yield return new WaitForSeconds(1.5f);
+
+                text.text = "";
+
+                yield return new WaitForSeconds(1.0f);
+            }
+    }
+    
+    IEnumerator MXText()
+    {
+        while (AnimationScript.isMX)
+        {
+                yield return new WaitForSeconds(0.5f);
+
+                text.text = "Ș̴͂T̶̖͂A̶̬̕T̴̘͑U̴̥̽S̶̟̃ : GET LUCAS";
+
+                yield return new WaitForSeconds(1.5f);
+
+                text.text = "Ș̴͂T̶̖͂A̶̬̕T̴̘͑U̴̥̽S̶̟̃ : ";
+
+                yield return new WaitForSeconds(1.0f);
+            }
+    }
+}
