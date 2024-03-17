d6719430c4465c1854eab10d2a301ffe30c38482
janidev20 <phjohn2008@gmail.com>
2024-03-17 10:30:19 +0100

    UPD 1 | MX & PipeCrawler anims
    yep

--- Assets/PlayerAnimation.cs
+++ Assets/PlayerAnimation.cs
@@ -7,13 +7,35 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] PlayerMovement plyMovement;
     [SerializeField] Animator playerAnimator;
 
+    [SerializeField] Animator MXAnimator, PCAnimator, FHAnimator;
+    [SerializeField] private GameObject MX_Body, PCrawler_Body, isFH_Body;
+
+    [SerializeField] private bool isMX, isPCrawler, isFH;
+
 
     private void Update()
+    {
+        AnimatorBooleans();
+        CharacterMethod();
+     
+        if (Input.GetKeyDown(KeyCode.F1) && !isMX)
+        {
+            isMX = true;
+            isPCrawler = false;
+        } else if (Input.GetKeyDown(KeyCode.F2) && !isPCrawler)
+        {
+            isPCrawler = true;
+            isMX = false;
+        }
+    }
+
+    void AnimatorBooleans()
     {
         if (plyMovement.isRunning)
         {
             playerAnimator.SetBool("isRunning", true);
-        } else
+        }
+        else
         {
             playerAnimator.SetBool("isRunning", false);
         }
@@ -21,7 +43,8 @@ public class PlayerAnimation : MonoBehaviour
         if (plyMovement.isJumping)
         {
             playerAnimator.SetBool("isJumping", true);
-        } else
+        }
+        else
         {
             playerAnimator.SetBool("isJumping", false);
         }
@@ -29,9 +52,37 @@ public class PlayerAnimation : MonoBehaviour
         if (plyMovement.isSliding)
         {
             playerAnimator.SetBool("isSliding", true);
-        } else
+        }
+        else
         {
             playerAnimator.SetBool("isSliding", false);
         }
     }
+
+    void CharacterMethod()
+    {
+        if (isMX)
+        {
+            playerAnimator = MXAnimator;
+            MX_Body.SetActive(true);
+            PCrawler_Body.SetActive(false);
+            isFH_Body.SetActive(false);
+        }
+        else if (isPCrawler)
+        {
+            playerAnimator = PCAnimator;
+            PCrawler_Body.SetActive(true);
+            MX_Body.SetActive(false);
+            isFH_Body.SetActive(false);
+        }
+        else if (isFH)
+        {
+            playerAnimator = FHAnimator;
+            MX_Body.SetActive(false);
+            PCrawler_Body.SetActive(false);
+            isFH_Body.SetActive(true);
+        }
+    }
+
+
 }
